88a29f46f51ebb5f20f09bb121d38bd59738ea97
Muhammadsami Patel <samipatel2092@gmail.com>
2026-04-24 01:01:32 +0530

    Add files via upload

---
+++ Overriding.py
@@ -0,0 +1,19 @@
+class vehicle:
+    def move(self):
+        print("Vehicle is moving")
+
+
+class Car(vehicle):
+    def move(self):
+        print("car driving on the road")
+
+
+class Bicycle(vehicle):
+    def move(self):
+        print(" bicycle pedalling on the road")
+
+
+vehicles = [Car(), Bicycle()]
+
+for v in vehicles:
+    v.move()
